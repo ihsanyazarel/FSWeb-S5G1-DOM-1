7f1ec2e466144fb51e2d5b17910103c9fc3bd8b0
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-06-16 22:50:13 +0300

    finished

--- src/index.js
+++ src/index.js
@@ -43,3 +43,52 @@ console.log('Proje açıldı!')
 
 
 /* Kodlar Buradan aşağıya */
+
+// header - nav- a
+const aTags = document.querySelectorAll("header nav a");
+for(let i = 0; i<aTags.length;i++){
+  aTags[i].className = "italic";
+  aTags[i].textContent = siteContent.nav["nav-item-" + (i+1)];
+}
+//logo img
+document.getElementById("logo-img").src = siteContent.images["logo-img"];
+// h1
+document.querySelector(".cta h1").textContent = siteContent.cta.h1;
+//button
+document.querySelector(".cta button").textContent = siteContent.cta.button;
+//cta img
+document.getElementById("cta-img").src = siteContent.images["cta-img"];
+
+// main-content h4
+const h4Tags = document.querySelectorAll(".main-content h4");
+h4Tags[0].textContent = siteContent["ana-içerik"]["özellikler-h4"];
+h4Tags[1].textContent = siteContent["ana-içerik"]["hakkımızda-h4"];
+h4Tags[2].textContent = siteContent["ana-içerik"]["servisler-h4"];
+h4Tags[3].textContent = siteContent["ana-içerik"]["ürünler-h4"];
+h4Tags[4].textContent = siteContent["ana-içerik"]["vizyon-h4"];
+
+//main-content p
+const pTags = document.querySelectorAll(".main-content p");
+pTags[0].textContent = siteContent["ana-içerik"]["özellikler-içerik"];
+pTags[1].textContent = siteContent["ana-içerik"]["hakkımızda-içerik"];
+pTags[2].textContent = siteContent["ana-içerik"]["servisler-içeriği"];
+pTags[3].textContent = siteContent["ana-içerik"]["ürünler-içeriği"];
+pTags[4].textContent = siteContent["ana-içerik"]["vizyon-içeriği"];
+
+// middle img
+document.getElementById("middle-img").src = siteContent.images["accent-img"];
+
+//contact h4
+const h4Tag = document.querySelector(".contact h4");
+h4Tag.textContent = siteContent.iletisim["iletişim-h4"];
+
+//contact p
+const cpntactPTags = document.querySelectorAll(".contact p");
+cpntactPTags[0].textContent = siteContent.iletisim.adres;
+cpntactPTags[1].textContent = siteContent.iletisim.telefon;
+cpntactPTags[2].textContent = siteContent.iletisim.email;
+
+// footer a
+const footerATag = document.getElementsByClassName("footer a");
+footerATag.className = "bold";
+footerATag.textContent = siteContent.footer.copyright;
